67efa17e8e8ec94a740a8939622173bc3c09652a
zohar <zoharkiks@gmail.com>
2021-07-20 13:05:01 +0530

    Added default bg image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,42 +11,36 @@ import fetchPhotos from "../utils/FetchPhoto";
 import fetchQuotes from "../utils/FetchQuotes";
 import { geolocated } from "react-geolocated";
 
-
-
-
-const Main = ({coords}) => {
-
-
-
-
-  
+const Main = ({ coords }) => {
   const [data, setData] = useState([]);
   const [city, setCity] = useState("");
   const [photos, setPhotos] = useState([]);
-  const [quote, setQuote] = useState()
+  const [quote, setQuote] = useState();
 
-  const getLatLong=()=>{
+  const getLatLong = () => {
     const { latitude, longitude } = coords;
-fetchWeather(setData,latitude,longitude)
-  }
- 
+    fetchWeather(setData, latitude, longitude);
+  };
+
   useEffect(() => {
-  
     fetchPhotos(`${data?.location?.name}`, setPhotos);
-    fetchQuotes(setQuote)
-    
+    fetchQuotes(setQuote);
   }, [data]);
 
   return (
     <main
       className="main relative before:absolute before:inset-0 before:bg-black/50  bg-no-repeat bg-cover bg-center lg:grid lg:grid-cols-12 "
-      style={{
+      style={data.length===0?{
+        backgroundImage:
+          `url(` +
+          `https://images.unsplash.com/photo-1512387331157-3e2a021998db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80` +
+          `)`,
+      }:{
         backgroundImage:
           `url(` +
           `https://live.staticflickr.com/${photos?.server}/${photos?.id}_${photos?.secret}_b.jpg` +
           `)`,
       }}
-    
     >
       <div className="   h-screen px-6 py-[2.5rem] flex flex-col justify-evenly  md:px-8 lg:col-span-6">
         <div className="relative flex flex-col ">
@@ -62,37 +56,49 @@ fetchWeather(setData,latitude,longitude)
               Another Location:
             </label>
             <div className="flex">
-            <input
-              id="location"
-              className="w-[50%] font-poppins mt-2 bg-transparent border-b-2  border-white outline-none text-white"
-              type="text"
-              onChange={(e) => {
-                setCity(e.target.value);
-              }}
-            />
-            <Icon onClick={getLatLong} icon={crosshairsGps} className='h-6 w-6 md:h-8 md:w-8  text-white ' />
+              <input
+                id="location"
+                className="w-[50%] font-poppins mt-2 bg-transparent border-b-2  border-white outline-none text-white"
+                type="text"
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+              />
+              <Icon
+                onClick={getLatLong}
+                icon={crosshairsGps}
+                className="h-6 w-6 md:h-8 md:w-8  text-white "
+              />
             </div>
-           
           </form>
         </div>
 
-<span  className='text-white font-poppins relative text-lg my-2 lg:text-xl italic'>"{quote?.content}"-{quote?.author}</span>
+        <span className="text-white font-poppins relative text-lg my-2 lg:text-xl italic">
+          "{quote?.content}"-{quote?.author}
+        </span>
 
         <div className=" relative flex flex-col  text-white font-poppins mb-15">
-          {data.length===0?<><span className='text-[3rem]'>Please enter a location</span></>:<><p>
-            <span className="text-[4rem]">{data?.current?.temp_c}°C </span>
-            <span className="text-[3rem] font-medium">
-              {data?.location?.name}
-            </span>
-          </p>
-          <span className="text-lg my-2 ">{data?.location?.localtime}</span>
-          <span className="text-lg ">{data?.current?.condition?.text}</span>
-          <img
-            src={`${data?.current?.condition?.icon}`}
-            className="h-20 w-20"
-            alt=""
-          /></>}
-          
+          {data.length === 0 ? (
+            <>
+              <span className="text-[3rem]">Please enter a location</span>
+            </>
+          ) : (
+            <>
+              <p>
+                <span className="text-[4rem]">{data?.current?.temp_c}°C </span>
+                <span className="text-[3rem] font-medium">
+                  {data?.location?.name}
+                </span>
+              </p>
+              <span className="text-lg my-2 ">{data?.location?.localtime}</span>
+              <span className="text-lg ">{data?.current?.condition?.text}</span>
+              <img
+                src={`${data?.current?.condition?.icon}`}
+                className="h-20 w-20"
+                alt=""
+              />
+            </>
+          )}
         </div>
 
         <Icon
@@ -100,7 +106,7 @@ fetchWeather(setData,latitude,longitude)
           className="animate-bounce h-12 w-12 text-white mx-auto lg:hidden relative  "
         />
       </div>
-      <div className=" lg:col-span-6" >
+      <div className=" lg:col-span-6">
         <Details
           cloud={data?.current?.cloud}
           humidity={data?.current?.humidity}
@@ -116,7 +122,7 @@ fetchWeather(setData,latitude,longitude)
 
 export default geolocated({
   positionOptions: {
-      enableHighAccuracy: false,
+    enableHighAccuracy: false,
   },
   userDecisionTimeout: 5000,
 })(Main);
--- src/utils/FetchWeather.jsx
+++ src/utils/FetchWeather.jsx
@@ -1,44 +1,33 @@
 import fetchPhotos from "../utils/FetchPhoto";
 
 const params = {
-    key: process.env.NEXT_PUBLIC_API_KEY,
-    baseUrl: "https://api.weatherapi.com/v1/",
-  };
+  key: process.env.NEXT_PUBLIC_API_KEY,
+  baseUrl: "https://api.weatherapi.com/v1/",
+};
 
+export const fetchWeather = async (setData, latitude, longitude) => {
+  await fetch(
+    `${params.baseUrl}forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${latitude},${longitude}&days=3&aqi=no&alerts=no`
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      setData(result);
+    });
+};
 
+export const fetchCity = async (city, e, setData, setCity, setPhotos) => {
+  e.preventDefault();
 
-
-  export const fetchWeather = async (setData,latitude,longitude) => {
-    
-
-    await fetch(
-      `${params.baseUrl}forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${latitude},${longitude}&days=3&aqi=no&alerts=no`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-      });
-  };
-
-  export const fetchCity = async (city,e,setData,setCity,setPhotos)=>{
-    e.preventDefault()
-
-    await fetch(
-      `${params.baseUrl}forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${city}&days=3&aqi=no&alerts=no`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-      });
-      Array.from(document.querySelectorAll("input")).forEach(
-        (input) => (input.value = "")
-      );
-      fetchPhotos(`${city}`,setPhotos)
-      setCity('')
-  }
-
-
-
- 
-
-
+  await fetch(
+    `${params.baseUrl}forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${city}&days=3&aqi=no&alerts=no`
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      setData(result);
+    });
+  Array.from(document.querySelectorAll("input")).forEach(
+    (input) => (input.value = "")
+  );
+  fetchPhotos(`${city}`, setPhotos);
+  setCity("");
+};
